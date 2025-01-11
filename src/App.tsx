16389b75dd765c0a2b8b851a13cf1f97981ca82f
Alex Pham <136045296+pham-15@users.noreply.github.com>
2025-01-10 19:45:30 -0600

    feat: add project folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import LandingPage from "./pages/LandingPage";
+import Planner from "./pages/Planner";
+import ChatBot from "./pages/ChatBot";
 
 const App = () => {
-  return <div className="bg-red-400 text-white">App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/planner" element={<Planner />} />
+        <Route path="/chatbot" element={<ChatBot />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LandingPage = () => {
+  return <div className="bg-red-400 text-white">LandingPage</div>;
+};
+
+export default LandingPage;
