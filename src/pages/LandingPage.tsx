7d53e7d3a9982fc04dfe6f13e3c03894057612ba
Shaz Kumar <shazk@Shazs-MacBook-Pro.local>
2025-03-10 20:38:23 -0500

    yet another scaling fix

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -7,20 +7,20 @@ const LandingPage = () => {
       <img
         src="/landing-line1.svg"
         alt="Landing"
-        className="absolute object-cover top-[47vh] left-[-1vw] w-[9vw]"
+        className="absolute object-cover top-[47vh] left-[-1vw] w-[8vw]"
       />
       <img
         src="/landing-line2.svg"
         alt="Landing"
-        className="absolute object-cover top-[47vh] left-[35vw] w-[45vw]"
+        className="absolute object-cover top-[47vh] left-[32vw] w-[40vw]"
       />
       <div className="pl-[10vw] z-10">
-        <h1 className="text-[#FFFFFF] text-[5.5vw] font-[Mermaid] font-bold text-left w-full">
+        <h1 className="text-[#FFFFFF] text-[5vw] font-[Mermaid] font-bold text-left w-full">
           Say hello to <span className="text-[#5AED87]">SAGE</span>, your <br />
           personal Al-powered <br />
           student advisor.
         </h1>
-        <div className="flex bg-[#181818] gap-4 rounded-l-full pr-8 absolute top-[calc(45vh+20vw)] left-[10vw]">
+        <div className="flex bg-[#181818] gap-4 rounded-l-full pr-8 absolute top-[calc(41vh+20vw)] left-[10vw]">
           <Link
             to="/"
             className="bg-[#5AED86] border-[#5AED86] border-2 text-black text-[1.1vw] scale-[50vw] px-8 py-3 rounded-full font-semibold hover:bg-transparent hover:text-white transition duration-300"
