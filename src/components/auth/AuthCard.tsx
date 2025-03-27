3a742b466076671b3115634a439ecefea795072a
jor-dango <jtan208295@gmail.com>
2025-03-26 21:26:43 -0500

    resized auth window based on zoom

--- src/components/auth/AuthCard.tsx
+++ src/components/auth/AuthCard.tsx
@@ -9,7 +9,7 @@ const AuthCard = (props: AuthCardProps) => {
   return (
     <div className="flex min-h-screen h-max dark justify-center [@media(max-height:720px)]:items-start items-center [@media(max-height:720px)]:py-24">
       <div className="[@media(max-height:720px)]:block hidden fixed top-0 left-0 w-screen h-16 bg-bgdark"></div>
-      <div className="flex w-[75vw] h-max min-h-[70vh] rounded-lg overflow-hidden">
+      <div className="flex w-[75vw] [@media(max-height:720px)]:w-[90vw] h-max min-h-[70vh] rounded-lg overflow-hidden">
         <div className="flex-[4] w-auto bg-[#E4E4E4] flex justify-center items-center px-4">
           <img
             src="/login_graphic.svg"
