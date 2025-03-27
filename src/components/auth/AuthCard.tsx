78c435a5ef8b8d34e87f28c64cef73a756529a4a
jor-dango <jtan208295@gmail.com>
2025-03-26 21:25:11 -0500

    minor padding changes to AuthCard

--- src/components/auth/AuthCard.tsx
+++ src/components/auth/AuthCard.tsx
@@ -10,7 +10,7 @@ const AuthCard = (props: AuthCardProps) => {
     <div className="flex min-h-screen h-max dark justify-center [@media(max-height:720px)]:items-start items-center [@media(max-height:720px)]:py-24">
       <div className="[@media(max-height:720px)]:block hidden fixed top-0 left-0 w-screen h-16 bg-bgdark"></div>
       <div className="flex w-[75vw] h-max min-h-[70vh] rounded-lg overflow-hidden">
-        <div className="flex-[4] w-auto bg-[#E4E4E4] flex justify-center items-center">
+        <div className="flex-[4] w-auto bg-[#E4E4E4] flex justify-center items-center px-4">
           <img
             src="/login_graphic.svg"
             alt="Login"
@@ -18,7 +18,7 @@ const AuthCard = (props: AuthCardProps) => {
           />
         </div>
         <div className="flex-[6] bg-bglight">
-          <div className="flex flex-col justify-center h-full w-full mx-auto px-[5rem]">
+          <div className="flex flex-col justify-center h-full w-full mx-auto px-[5rem] py-8">
             <h3 className="text-[24px] font-bold text-textdark mb-8">
               {props.title}
             </h3>
