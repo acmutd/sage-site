a935303a2144be07d55ad3e0b00ba4154a7eaca6
Alen Jo <jo.alen1@outlook.com>
2026-01-27 16:10:23 -0600

    Dev Env distinction + Toast Message Fix
    - bug in the toast message thing not initialized; therefore, this is added and fixed
    - since we're most likely going to be conducting QA/UAT for just ACM in general, I figured it'd be a good idea to have a "Development Environment" branding to allow for internal testing). The motivation for this comes from how Vercel doesn't allow for "specific people" to access a particular URL without it being exposed to everyone...therefore, to add safeguards in place for particular types of branches to test OR just dev itself, functionality is added for this

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,21 @@ import { Menu, MessageCirclePlus, Route, UserRound} from "lucide-react";
 import { useEffect, useState } from "react";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
+// check environment
+const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT as string | undefined;
+
 const Navbar = () => {
   const { user, logout } = useAuth();
   const [isInWebapp, setIsInWebapp] = useState(false);
   const [profilePicture, setProfilePicture] = useState<string>("");
 
   let location = useLocation().pathname;
+
+  useEffect(() => {
+    const navHeight = ENVIRONMENT === 'development' ? '6rem' : '4.2rem';
+    document.documentElement.style.setProperty('--nav-height', navHeight);
+  }, []);
+
   useEffect(() => {
     if (location === "/" || location === "/login" || location === "/signup" || location === "/forgot-password") {
       setIsInWebapp(false);
@@ -46,102 +55,113 @@ const Navbar = () => {
     <AuthProvider>
 
       {/* Standard navbar */}
-      <nav className={`
-        ${isInWebapp ? "bg-bglight border-b-[1px] shadow-sm" : undefined} 
-        py-2.5 px-6 fixed w-full z-10 hidden md:block
+      <>
+        {ENVIRONMENT === 'development' && (
+            <div className="fixed top-0 left-0 right-0 h-4 bg-purple-600 text-white text-center text-xs font-medium z-[200] shadow-sm flex items-center justify-center">
+              Dev Environment
+            </div>
+        )}
+
+        <nav className={`
+          ${isInWebapp ? "bg-bglight border-b-[1px] shadow-sm" : undefined} 
+          py-2.5 px-6 fixed w-full z-10 hidden md:block
+          ${ENVIRONMENT === 'development' ? 'top-4' : 'top-0'}
         `}>
-        <div className="flex items-center justify-between w-full">
-          <Link to="/" className="ml-0">
-            <img src={isInWebapp ? "/Sage_Logo_Dark.svg" : "/Sage_Logo_Light.svg"} alt="SAGE" className="h-8 w-auto" />
-          </Link>
-          <ul className="flex items-center space-x-6 mr-0">
-            <li className="flex-row">
-              <Link
-                to="/planner"
-                className={`${isInWebapp ? "text-textdark hover:text-gray-500" : "text-textlight hover:text-gray-200"}
-                flex items-center gap-2`}
-              >
-                <Route className="stroke-accent" />
-                Plan your degree
-              </Link>
-            </li>
-            <li className="flex-row">
-              <Link
-                to="/chatbot"
-                className={`${isInWebapp ? "text-textdark hover:text-gray-500" : "text-textlight hover:text-gray-200"}
-                flex items-center gap-2 hover:text-gray-200"`}
-              >
-                <MessageCirclePlus className="stroke-accent" />
-                Start a chat
-              </Link>
-            </li>
-            <li>
-              {user ? (
-                // If user is logged in, show Sign Out button
-                // <button
-                //   onClick={logout} // Calls logout function
-                //   className="bg-destructive text-textlight text-base px-6 py-1.5 rounded-full font-semibold hover:bg-red-700 transition duration-300"
-                // >
-                //   Sign Out
-                // </button>
-                
-                //if user is loggin in, show menu icon
-                <DropdownMenu>
-                  <DropdownMenuTrigger>
-                    <div className="${!profilePicture ? 'bg-secondary' : ''} p-2 rounded-full">
-                    {profilePicture ? (
-                      <img 
-                        src={profilePicture} 
-                        alt="Profile" 
-                        className="w-9 h-9 rounded-full object-cover justify-center"
-                      />
-                    ) : (
-                      <UserRound className="stroke-textdark"/>
-                    )}
-                    </div>
-                  </DropdownMenuTrigger>
-                    <DropdownMenuContent
-                      className={`bg-bglight flex flex-col p-2 gap-2 mr-6 items-center rounded-sm`}
-                    >
-                      <DropdownMenuItem className="focus:bg-innercontainer w-full">
-                      <Link
-                      to="/profile"
-                      className={`text-textdark hover:text-gray-700 flex flex-row w-full justify-start items-center gap-2`}
-                      >
-                      <UserRound className="stroke-accent" />
-                      Your Profile
-                    </Link>
-                      </DropdownMenuItem>
-
-                      <DropdownMenuItem className="focus:bg-innercontainer w-full">
-                        <button
-                          onClick={logout} // Calls logout function
-                          className="bg-destructive text-textlight text-base px-6 py-1.5 rounded-full font-semibold hover:bg-red-700 transition duration-300"
-                        >
-                          Sign Out
-                        </button>
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-              ) : (
-                // If no user, show Login button
+            
+          <div className="flex items-center justify-between w-full">
+            <Link to="/" className="ml-0">
+              <img src={isInWebapp ? "/Sage_Logo_Dark.svg" : "/Sage_Logo_Light.svg"} alt="SAGE" className="h-8 w-auto" />
+            </Link>
+            <ul className="flex items-center space-x-6 mr-0">
+              <li className="flex-row">
                 <Link
-                  to="/login"
-                  className="bg-accent text-textdark text-base px-8 py-3 rounded-full font-semibold hover:bg-buttonhover transition duration-300"
+                  to="/planner"
+                  className={`${isInWebapp ? "text-textdark hover:text-gray-500" : "text-textlight hover:text-gray-200"}
+                  flex items-center gap-2`}
                 >
-                  Login
+                  <Route className="stroke-accent" />
+                  Plan your degree
                 </Link>
-              )}
-            </li>
-          </ul>
-        </div>
-      </nav>
+              </li>
+              <li className="flex-row">
+                <Link
+                  to="/chatbot"
+                  className={`${isInWebapp ? "text-textdark hover:text-gray-500" : "text-textlight hover:text-gray-200"}
+                  flex items-center gap-2 hover:text-gray-200"`}
+                >
+                  <MessageCirclePlus className="stroke-accent" />
+                  Start a chat
+                </Link>
+              </li>
+              <li>
+                {user ? (
+                  // If user is logged in, show Sign Out button
+                  // <button
+                  //   onClick={logout} // Calls logout function
+                  //   className="bg-destructive text-textlight text-base px-6 py-1.5 rounded-full font-semibold hover:bg-red-700 transition duration-300"
+                  // >
+                  //   Sign Out
+                  // </button>
+                  
+                  //if user is loggin in, show menu icon
+                  <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <div className="${!profilePicture ? 'bg-secondary' : ''} p-2 rounded-full">
+                      {profilePicture ? (
+                        <img 
+                          src={profilePicture} 
+                          alt="Profile" 
+                          className="w-9 h-9 rounded-full object-cover justify-center"
+                        />
+                      ) : (
+                        <UserRound className="stroke-textdark"/>
+                      )}
+                      </div>
+                    </DropdownMenuTrigger>
+                      <DropdownMenuContent
+                        className={`bg-bglight flex flex-col p-2 gap-2 mr-6 items-center rounded-sm`}
+                      >
+                        <DropdownMenuItem className="focus:bg-innercontainer w-full">
+                        <Link
+                        to="/profile"
+                        className={`text-textdark hover:text-gray-700 flex flex-row w-full justify-start items-center gap-2`}
+                        >
+                        <UserRound className="stroke-accent" />
+                        Your Profile
+                      </Link>
+                        </DropdownMenuItem>
 
-      {/* Dropdown navbar when screen width < md (768px) */}
+                        <DropdownMenuItem className="focus:bg-innercontainer w-full">
+                          <button
+                            onClick={logout} // Calls logout function
+                            className="bg-destructive text-textlight text-base px-6 py-1.5 rounded-full font-semibold hover:bg-red-700 transition duration-300"
+                          >
+                            Sign Out
+                          </button>
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                  </DropdownMenu>
+                ) : (
+                  // If no user, show Login button
+                  <Link
+                    to="/login"
+                    className="bg-accent text-textdark text-base px-8 py-2 rounded-full font-semibold hover:bg-buttonhover transition duration-300"
+                  >
+                    Login
+                  </Link>
+                )}
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </>
+
+      {/* Mobile Navbar -- Dropdown navbar when screen width < md (768px) */}
       <nav className={`
         ${isInWebapp ? "bg-bglight border-b-[1px] shadow-sm" : undefined} 
         py-4 px-6 fixed w-full h-[4.2rem] z-10 md:hidden block
-        `}>
+        ${ENVIRONMENT === 'development' ? 'top-4' : 'top-0'}
+      `}>
         <div className="flex items-center justify-between w-full">
           <Link to="/" className="ml-0">
             <img src={isInWebapp ? "/Sage_Logo_Dark.svg" : "/Sage_Logo_Light.svg"} alt="SAGE" className="h-8 w-auto" />
