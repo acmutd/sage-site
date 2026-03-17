842b7fa8845dbd687c446641d4375795adfa2852
Alen Jo <jo.alen1@outlook.com>
2026-03-17 12:56:24 -0500

    Retain back Dev Navbar
    - my bad...need this back for the testers...local branch I should remove it for demos

--- src/components/PlannerNavbar.tsx
+++ src/components/PlannerNavbar.tsx
@@ -91,7 +91,7 @@ const PlannerNavbar: React.FC<PlannerNavbarProps> = ({
   return (
     <>
       {/* Desktop navbar */}
-      {ENVIRONMENT !== 'development' && (
+      {ENVIRONMENT === 'development' && (
         <div className="fixed top-0 left-0 right-0 h-4 bg-purple-600 text-white text-center text-xs font-medium z-[200] shadow-sm flex items-center justify-center">
           Dev Environment
         </div>
@@ -100,7 +100,7 @@ const PlannerNavbar: React.FC<PlannerNavbarProps> = ({
       <nav className={`
         ${isInWebapp ? "bg-bglight border-b-[1px] shadow-sm" : undefined} 
         py-2.5 px-6 fixed w-full z-10 hidden md:block
-        ${ENVIRONMENT !== 'development' ? 'top-4' : 'top-0'}
+        ${ENVIRONMENT === 'development' ? 'top-4' : 'top-0'}
       `}>
         <div className="flex items-center justify-between w-full">
           <div className="flex items-center gap-4">
